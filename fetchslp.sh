1bca04a96e307d9de0976255d8b377948b387458
gerlevodka <50401746+gerlevodka@users.noreply.github.com>
2021-03-03 18:13:30 +0100

    use script

---
+++ fetchslp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git clone https://github.com/MentalOutlaw/dmenu.git
+git clone https://github.com/MentalOutlaw/st.git
+git clone https://github.com/MentalOutlaw/slstatus.git
+git clone https://github.com/MentalOutlaw/dwm.git
